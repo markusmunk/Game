7930eb6d6ff83108e95b5d33fc32ebc4016f7b1d
Theis Agerskov Tengs <theistengs@gmail.com>
2019-11-16 21:44:20 +0100

    Auto stash before merge of "Rooms" and "origin/Rooms"

---
+++ gameJam2019/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    Vector3 distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.Find("Player").GetComponent<Transform>();
+        distance = player.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position - distance;
+    }
+}
