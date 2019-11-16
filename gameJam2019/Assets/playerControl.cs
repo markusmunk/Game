c8afa963eac3cba695b4bbf2ccb501f05786b386
Markus Munk <mmunk19@student.sdu.dk>
2019-11-16 11:48:00 +0100

    En unity fil at starte med
    adasd

---
+++ gameJam2019/Assets/playerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Player shuld go left
+            rb.AddForce(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+        }
+    }
+}
